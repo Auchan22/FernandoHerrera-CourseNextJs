2d4c0686c52a82b5367ede1452ec50a5f2bd5a54
Agustin <agustinsurila@gmail.com>
2022-08-10 20:06:25 -0300

    Initial commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import db from '../../db/db.json';
 
-type Data = {
-  name: string
+interface Data {
+  id: number;
+  name: string;
+  edad: number;
 }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data[]>,
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(db);
 }
